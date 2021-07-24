564051aa1f935521236ec9c2fd3d44572eb815ec
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-07-24 10:32:45 +0430

    add AOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import './App.css';
 
+
 function App() {
+
+    useEffect(()=>{
+        AOS.init()
+        AOS.refresh()
+    },[])
   return (
     <>
-      <p className={"text-red-800 text-9xl"}>test</p>
+      <p className={"text-red-800 text-9xl"} data-aos={"fade-up"}>test</p>
     </>
   );
 }
