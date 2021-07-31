8d10b3eb4f3679e624addb78ca9811a0e7c8db38
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-07-31 15:39:41 +0430

    add user account to dashboard

--- src/components/market/userDashboard/userAccount/userAccount.tsx
+++ src/components/market/userDashboard/userAccount/userAccount.tsx
@@ -36,17 +36,17 @@ function UserAccount() {
                             <input className={"w-7/12 outline-none p-1 border-2 mr-1"} dir={"ltr"} type={"password"}/>
                         </div>
 
-                        <div className={"w-full flex items-center justify-center md:mt-2"}>
+                        <div className={"w-full flex items-center justify-center mt-2"}>
                             <label className={"w-4/12 text-right"}>رمز عبور جدید</label>
                             <input className={"w-7/12 outline-none p-1 border-2 mr-1"} dir={"ltr"} type={"password"}/>
                         </div>
 
-                        <div className={"w-full flex items-center justify-center mt-2"}>
+                        <div className={"w-full flex items-center justify-center md:mt-2"}>
                             <label className={"w-4/12 text-right"}>تکرار رمز عبور جدید</label>
                             <input className={"w-7/12 outline-none p-1 border-2 mr-1"} dir={"ltr"} type={"password"}/>
                         </div>
                     </div>
-                    <button className={"mt-2 p-2 bg-green-600 rounded-lg text-white"}>تغییر رمز</button>
+                    <button className={"mt-2 p-2 bg-green-600 rounded-lg text-white"}>تغییر رمز عبور</button>
                 </div>
             </div>
         </>
