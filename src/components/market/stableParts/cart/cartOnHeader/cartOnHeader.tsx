69587e2d75f6baedbb3adced3628a08e63e39efb
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-08-10 21:40:00 +0430

    add header Cart

---
+++ src/components/market/stableParts/cart/cartOnHeader/cartOnHeader.tsx
@@ -0,0 +1,61 @@
+import {Link} from 'react-router-dom'
+
+function CartOnHeader() {
+    return(
+        <div className={"w-full h-full rounded border-gray-300 border-2 bg-gray-200 font-anjoman py-2 px-1"}>
+            <div className={"w-full flex flex-col bg-transparent"}>
+                <Link to={'/product/ca2pd1'}>
+                    <div className={"w-full flex border-gray-100 border-b-2 py-2 px-2 hover:bg-gray-100"}>
+                        <div className={"w-9/12"}>
+                            <p className={"w-full text-sm text-gray-700"}>
+                                لپ تاپ ایسوس TUF Dash F15 FX516 Core i7/8GB/512GB SSD/4GB
+                            </p>
+                            <p className={"w-full text-xs text-gray-400"}>3 عدد</p>
+                        </div>
+                        <div className={"w-3/12 max-h-14 mr-1"}>
+                            <img className={"w-full h-full"} src={'https://www.lioncomputer.com/uploads/image/2021/7/thumbnail/1626940785-608KkzZwO8YQdzZq.webp'}/>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to={'/product/ca2pd1'}>
+                    <div className={"w-full flex border-gray-100 border-b-2 py-2 px-2 hover:bg-gray-100"}>
+                        <div className={"w-9/12"}>
+                            <p className={"w-full text-sm text-gray-700"}>
+                                لپ تاپ ایسوس TUF Dash F15 FX516 Core i7/8GB/512GB SSD/4GB
+                            </p>
+                            <p className={"w-full text-xs text-gray-400"}>3 عدد</p>
+                        </div>
+                        <div className={"w-3/12 max-h-14 mr-1"}>
+                            <img className={"w-full h-full"} src={'https://www.lioncomputer.com/uploads/image/2021/7/thumbnail/1626940785-608KkzZwO8YQdzZq.webp'}/>
+                        </div>
+                    </div>
+                </Link>
+
+                <Link to={'/product/ca2pd1'}>
+                    <div className={"w-full flex border-gray-100 border-b-2 py-2 px-2 hover:bg-gray-100"}>
+                        <div className={"w-9/12"}>
+                            <p className={"w-full text-sm text-gray-700"}>
+                                لپ تاپ ایسوس TUF Dash F15 FX516 Core i7/8GB/512GB SSD/4GB
+                            </p>
+                            <p className={"w-full text-xs text-gray-400"}>3 عدد</p>
+                        </div>
+                        <div className={"w-3/12 max-h-14 mr-1"}>
+                            <img className={"w-full h-full"} src={'https://www.lioncomputer.com/uploads/image/2021/7/thumbnail/1626940785-608KkzZwO8YQdzZq.webp'}/>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+            <div className={"w-full px-2 mt-2"}>
+                <p className={"text-center"}>
+                    <p className={"text-sm text-gray-500"}>قابل پرداخت :</p>
+                    <p className={"text-gray-700"}>352000000 تومان</p>
+                </p>
+                <button className={"w-full ring-red-300 ring-2 hover:bg-red-300 rounded py-2 text-sm text-gray-700 mt-2"}>خالی کردن سبد خرید</button>
+                <button className={"w-full ring-green-600 ring-2 bg-green-500 hover:bg-green-600 rounded py-2 text-gray-200 mt-2"}>ثبت سفارش نهایی</button>
+            </div>
+        </div>
+    )
+}
+
+export default CartOnHeader
--- src/components/market/stableParts/header/header.tsx
+++ src/components/market/stableParts/header/header.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useState} from "react";
 import { FaSearch , FaUser , FaShoppingCart , FaBars } from "react-icons/fa";
 import DesktopCategory from "../desktopCategory/desktopCategory";
+import CartOnHeader from "../cart/cartOnHeader/cartOnHeader";
 
 function Header (props:{setOffCanvShow:Function}) {
 
     const [categoryShow , setCategoryShow] = useState(false)
+    const [headerCartShow , setHeaderCartShow] = useState(false)
 
     return(
         <header className={"w-full h-auto px-5 lg:px-36 pt-5 pb-3 shadow-md bg-white sticky top-0 z-50"}>
@@ -16,7 +18,7 @@ function Header (props:{setOffCanvShow:Function}) {
                         <p className={"text-2xl md:text-3xl font-normal text-gray-800 ml-1"}>Market</p>
                     </div>
                     <div className={"self-center"}>
-                        <FaBars className={"h-full block md:hidden text-xl cursor-pointer"} onClick={()=> props.setOffCanvShow(true)}></FaBars>
+                        <FaBars className={"h-full block md:hidden text-xl cursor-pointer"} onClick={()=> props.setOffCanvShow(true)}/>
                     </div>
                 </div>
                 <div className={"w-full h-11 flex justify-between md:mr-10"}>
@@ -24,23 +26,27 @@ function Header (props:{setOffCanvShow:Function}) {
                         <FaSearch className={"h-full text-2xl text-gray-400"}/>
                         <input placeholder={"جستجو ..."} className={"w-full px-2 bg-transparent placeholder-gray-400 text-gray-500 font-anjoman outline-none"}/>
                     </div>
-                    <div className={"flex"}>
-                        <div className={"pl-3 border-l-2 ml-3"}>
+                    <div className={"relative flex"} onMouseLeave={()=> setHeaderCartShow(false)}>
+                        <div className={"pl-3 border-l-2 ml-3 mb-1"}>
                             <div className={"h-full flex justify-center items-center border hover:border-gray-500 cursor-pointer rounded text-gray-400 hover:text-gray-600 p-2 text-center"}>
                                 <p className={"h-full font-anjoman pl-3 hidden lg:block"}>ورود به حساب کاربری</p>
                                 <FaUser className={"text-xl"}/>
                             </div>
                         </div>
-                        <FaShoppingCart className={"text-4xl h-full text-green-500 cursor-pointer"}/>
+                        <div className={"relative"}>
+                            <div className={`${false && 'hidden'} absolute top-0 right-0 bg-red-400 px-1 rounded-full text-gray-100`}>5</div>
+                            <FaShoppingCart className={"text-4xl h-full text-green-500 cursor-pointer"} onMouseEnter={()=> setHeaderCartShow(true)}/>
+                        </div>
+                        <div className={`${!headerCartShow && 'hidden'} absolute w-full left-0 top-11`}><CartOnHeader/></div>
                     </div>
                 </div>
             </div>
 
-            <div className={"hidden relative md:flex font-anjoman font-thin text-gray-500"} onMouseEnter={()=> setCategoryShow(true)} onMouseLeave={()=> setCategoryShow(false)}>
+            <div className={"hidden relative w-1/2 md:flex font-anjoman font-thin text-gray-500"} onMouseEnter={()=> setCategoryShow(true)} onMouseLeave={()=> setCategoryShow(false)}>
                 <div className={"flex pb-1 border-l-2 pl-3 ml-3"}>
                     <button className={"hover:text-green-500"}>دسته بندی کالا ها</button>
                 </div>
-                <div className={`${!categoryShow && 'hidden'} absolute w-3/6 md:w-2/6 top-7 h-50`}><DesktopCategory/></div>
+                <div className={`${!categoryShow && 'hidden'} absolute w-full top-7 h-50`}><DesktopCategory/></div>
                 <button className={"hover:text-green-500 text-sm ml-8"}>لپ تاپ ها</button>
                 <button className={"hover:text-green-500 text-sm ml-8"}>کارت گرافیک ها</button>
                 <button className={"hover:text-green-500 text-sm ml-8"}>هدفون ها</button>
