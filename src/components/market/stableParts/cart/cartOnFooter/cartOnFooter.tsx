86577db69aececad33f31b8e593e3b988490a8cf
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-08-11 22:19:36 +0430

    make cartOnFooter dynamic

--- src/components/market/stableParts/cart/cartOnFooter/cartOnFooter.tsx
+++ src/components/market/stableParts/cart/cartOnFooter/cartOnFooter.tsx
@@ -1,12 +1,32 @@
 import {Link} from 'react-router-dom'
+import {IcartItem} from "../../../../../interfaces/redux";
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 
 function CartOnFooter() {
+
+    const cartItemsRedux: IcartItem[] = useSelector((state: any) => state.cart.cartList)
+
+    const [finalPrice , setFinalPrice] = useState<number>(0)
+
+    useEffect(()=>{
+        let fullPrice = 0
+        for (let i = 0; i < cartItemsRedux.length; i++) {
+            const newPrice = (cartItemsRedux[i].price - (cartItemsRedux[i].price * cartItemsRedux[i].offPercent)) * cartItemsRedux[i].number
+            fullPrice = fullPrice + newPrice
+        }
+        setFinalPrice(fullPrice)
+    },[cartItemsRedux])
+
     return(
         <div className={"w-full bg-white rounded-t-xl border-b-0 border-green-200 border-2 px-5 font-anjoman"}>
             <div className={"flex py-2"}>
                 <p className={"w-6/12"}>
                     <p className={"text-sm text-gray-500"}>قابل پرداخت :</p>
-                    <p className={"text-gray-700"}>352000000 تومان</p>
+                    <p className={"text-gray-700"}>
+                        <span>{finalPrice} </span>
+                        <span>تومان</span>
+                    </p>
                 </p>
 
                 <div className={"w-6/12"}>
