291084961742ba2b0c591500b52bbf945db8bb4d
Ali Kooshesh <Alikooshesh007@gmail.com>
2021-08-12 13:36:48 +0430

    add signUp box

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import axios from "axios";
+import {Redirect} from "react-router";
 
 function LoginPage() {
 
     const [box,setBox] = useState<number>(1)
     const [phoneNumber , setPhoneNumer] = useState<number|string>("")
     const [code , setCode] = useState<number|string>("")
+    const [fullName , setFullName] = useState<string>("")
     const [loading,setLoading] = useState<boolean>(false)
 
     function phoneInputChangeHandle(e:React.ChangeEvent<HTMLInputElement>) {
@@ -46,7 +48,12 @@ function LoginPage() {
             axios.post('https://pcmarket-server-api.herokuapp.com/userLogin/verifyCode',{phoneNumber : phoneNumber , verifyCode : code})
                 .then(res => {
                     console.log(res.data)
-                    setLoading(false)
+                    if (res.data.fullName){
+                        return <Redirect to={'/dashboard'}/>
+                    }else {
+                        setBox(3)
+                        setLoading(false)
+                    }
                 })
                 .catch(err => {
                     console.log(err)
@@ -55,16 +62,17 @@ function LoginPage() {
         }
     }
 
+
     return(
-        <div className={"w-full h-full flex justify-center items-center bg-gray-300"}>
-            <div className={`${box != 1 && 'hidden'} w-1/4 bg-gray-200 rounded-lg px-3 py-2 text-center font-anjoman text-gray-600`}>
+        <div className={"w-full h-full flex justify-center items-center bg-gray-300 p-3"}>
+            <div className={`${box != 1 && 'hidden'} w-full md:w-2/4 lg:w-2/5 xl:w-1/4 bg-gray-200 rounded-lg px-3 py-2 text-center font-anjoman text-gray-600`}>
                 <label htmlFor={"phone"}>شماره موبایلتان را وارد کنید</label>
                 <input id={"phone"} placeholder={"09120000000"} className={"w-full h-8 p-2 text-center mt-1"} value={phoneNumber} onChange={phoneInputChangeHandle}/>
 
                 <button className={`w-2/3 h-9 bg-green-500 hover:bg-green-600 rounded-lg mt-2 text-gray-100`} onClick={sendCodeBtnClick}>ارسال کد یک بار مصرف</button>
             </div>
 
-            <div className={`${box != 2 && 'hidden'} w-1/4 bg-gray-200 rounded-lg px-3 py-2 text-center font-anjoman text-gray-600`}>
+            <div className={`${box != 2 && 'hidden'} w-full md:w-2/4 lg:w-2/5 xl:w-1/4 bg-gray-200 rounded-lg px-3 py-2 text-center font-anjoman text-gray-600`}>
                 <div className={"w-full flex justify-between items-center"}>
                     <p>{`شماره تلفن شما : ${phoneNumber}`}</p>
                     <button className={"text-blue-400 text-xs"} onClick={()=> setBox(1)}>تغییر شماره</button>
@@ -74,6 +82,18 @@ function LoginPage() {
 
                 <button className={"w-2/3 h-9 border-red-400 border-1 hover:bg-red-400 rounded-lg mt-2 text-red-400 hover:text-gray-200"}>ارسال مجدد</button>
                 <button className={"w-2/3 h-9 bg-green-500 hover:bg-green-600 border-green-500 border-1 rounded-lg mt-2 text-gray-100"} onClick={verifyCodeBtnClick}>ورود</button>
+            </div>
+
+            <div className={`${box != 3 && 'hidden'} w-full md:w-2/4 lg:w-2/5 xl:w-1/4 bg-gray-200 rounded-lg px-3 py-2 text-center font-anjoman text-gray-600`}>
+                <div className={"w-full flex justify-between items-center"}>
+                    <p>{`شماره تلفن شما : ${phoneNumber}`}</p>
+                    <button className={"text-blue-400 text-xs"} onClick={()=> setBox(1)}>تغییر شماره</button>
+                </div>
+                <label htmlFor={"fullName"} className={"mt-3"}>نام و نام خانوادگیتان را وارد کنید</label>
+                <input id={"fullName"} placeholder={"مهران مدیری"} className={"w-full h-8 p-2 text-center mt-1"}
+                       value={fullName} onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setFullName(e.target.value)}/>
+
+                <button className={"w-2/3 h-9 bg-green-500 hover:bg-green-600 border-green-500 border-1 rounded-lg mt-2 text-gray-100"} onClick={verifyCodeBtnClick}>ورود به داشبورد</button>
             </div>
         </div>
     )
